ea91bd20cf49e27967fde54e2c2acbc2de6aaa8c
t.kawaba <t.kawaba@gmail.com>
2026-06-20 12:21:41 +0900

    Update setup.bat
    調整

--- portable-python-vscode/setup.bat
+++ portable-python-vscode/setup.bat
@@ -1,10 +1,11 @@
+@echo off
 powershell.exe -ExecutionPolicy Bypass -Command "Unblock-File -Path '%~dp0setup.ps1'"
 powershell.exe -ExecutionPolicy Bypass -File "%~dp0setup.ps1"
 
-if %ERRORLEVEL% NEQ 0 (
-    echo --- セットアップは中止されました ---
+if errorlevel 1 (
+    echo --- Setup canceled ---
     pause
-    exit /b %ERRORLEVEL%
+    exit /b 1
 )
 
 echo --- Script finished ---
